65d8c4cca3d9ecf8f68f62df454bb291914c6646
Andrey <isaac220515@gmail.com>
2019-11-22 13:28:59 +0300

    fix bugs
    1)incorrect folder view
    2)changed view tab from 8 spaces to 2

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './src/redux/store';
-import { View } from 'native-base';
-import RootComponent from './src/RootComponent';
-import { ActivityIndicator } from 'react-native';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./src/redux/store";
+import { View } from "native-base";
+import RootComponent from "./src/RootComponent";
+import { ActivityIndicator } from "react-native";
 
 class App extends Component {
 	render(): React.ReactElement {
